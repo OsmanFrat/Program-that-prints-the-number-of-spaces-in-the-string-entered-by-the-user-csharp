40d00c99d6eb2a92619c6ce5f0d9b54e1415abaf
Ozu <osmanfrat@yahoo.com>
2021-12-20 20:49:14 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using static System.Console;
+
+
+namespace metotlar
+{
+    public class deneme
+    {
+        public static void Main(string[] args)
+        {
+            int toplam = 0;
+            WriteLine("Please input a string: ");
+            string yazi = ReadLine();
+
+            foreach(char x in yazi)
+            {
+                if(x == ' ')
+                {
+                    toplam = toplam + 1;
+                }
+            }
+            WriteLine(yazi + " contains " + toplam + " spaces.");
+        }
+       
+    }
+}
